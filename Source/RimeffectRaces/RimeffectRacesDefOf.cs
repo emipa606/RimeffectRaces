099fe768841e2c9e6ad4d08f326d23b68175bfe9
troopersmith1 <62363949+troopersmith1@users.noreply.github.com>
2020-09-21 00:24:10 -0400

    Add files via upload

---
+++ Source/RimeffectRaces/RimeffectRacesDefOf.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+using RimWorld;
+using Verse;
+
+namespace RimeffectRaces
+{
+    [DefOf]
+    public static class RimeffectRacesDefOf
+    {
+        static RimeffectRacesDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(RimeffectRacesDefOf));
+        }
+
+        public static FactionDef AsariFaction;
+
+        public static FactionDef BatarianFaction;
+
+        public static FactionDef EnkindlerFaction;
+
+        public static FactionDef Geth;
+
+        public static FactionDef KroganFaction;
+
+        public static FactionDef QuarianFaction;
+
+        public static FactionDef Reapers;
+
+        public static FactionDef SalarianFaction;
+
+        public static FactionDef TurianFaction;
+
+        public static FactionDef VorchaFaction;
+
+        public static FactionDef WhiteHandFaction;
+
+    }
+
+}
---
+++ Source/RimeffectRaces/RimeffectRacesFactionControl.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+using RimWorld;
+using Verse;
+
+namespace RimeffectRaces
+{
+    [StaticConstructorOnStartup]
+    public static class FactionControl
+    {
+        static FactionControl()
+        {
+            RimeffectRacesSettings settings = RimeffectRacesMod.mod.settings;
+
+            if (!settings.AsariFaction)
+            {
+                RimeffectRacesDefOf.AsariFaction.requiredCountAtGameStart = 0;
+                RimeffectRacesDefOf.AsariFaction.maxCountAtGameStart = 0;
+            }
+
+            if (!settings.BatarianFaction)
+            {
+                RimeffectRacesDefOf.BatarianFaction.requiredCountAtGameStart = 0;
+                RimeffectRacesDefOf.BatarianFaction.maxCountAtGameStart = 0;
+            }
+
+            if (!settings.EnkindlerFaction)
+            {
+                RimeffectRacesDefOf.EnkindlerFaction.requiredCountAtGameStart = 0;
+                RimeffectRacesDefOf.EnkindlerFaction.maxCountAtGameStart = 0;
+            }
+
+            if (!settings.KroganFaction)
+            {
+                RimeffectRacesDefOf.KroganFaction.requiredCountAtGameStart = 0;
+                RimeffectRacesDefOf.KroganFaction.maxCountAtGameStart = 0;
+            }
+
+            if (!settings.QuarianFaction)
+            {
+                RimeffectRacesDefOf.QuarianFaction.requiredCountAtGameStart = 0;
+                RimeffectRacesDefOf.QuarianFaction.maxCountAtGameStart = 0;
+            }
+
+            if (!settings.SalarianFaction)
+            {
+                RimeffectRacesDefOf.SalarianFaction.requiredCountAtGameStart = 0;
+                RimeffectRacesDefOf.SalarianFaction.maxCountAtGameStart = 0;
+            }
+
+            if (!settings.TurianFaction)
+            {
+                RimeffectRacesDefOf.TurianFaction.requiredCountAtGameStart = 0;
+                RimeffectRacesDefOf.TurianFaction.maxCountAtGameStart = 0;
+            }
+
+            if (!settings.VorchaFaction)
+            {
+                RimeffectRacesDefOf.VorchaFaction.requiredCountAtGameStart = 0;
+                RimeffectRacesDefOf.VorchaFaction.maxCountAtGameStart = 0;
+            }
+
+            if (!settings.Geth)
+            {
+                RimeffectRacesDefOf.Geth.requiredCountAtGameStart = 0;
+                RimeffectRacesDefOf.Geth.maxCountAtGameStart = 0;
+            }
+
+            if (!settings.Reapers)
+            {
+                RimeffectRacesDefOf.Reapers.requiredCountAtGameStart = 0;
+                RimeffectRacesDefOf.Reapers.maxCountAtGameStart = 0;
+            }
+
+        }
+    }
+}
