beeac5ef411fffb818b6ee9d2e95fc15944219a2
troopersmith1 <62363949+troopersmith1@users.noreply.github.com>
2021-09-12 15:21:47 -0400

    Add files via upload

---
+++ 1.3/Source/RimeffectRaces/BloodRage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace RimeffectRaces
+{
+    public class BloodRageGiver : HediffGiver
+    {
+        public bool HarmedRecently(Pawn pawn) => Find.TickManager.TicksGame - ((Pawn_MindState)pawn.mindState).lastHarmTick < 2500;
+
+        public override void OnIntervalPassed(Pawn pawn, Hediff cause)
+        {
+            if (!this.HarmedRecently(pawn))
+                return;
+            HealthUtility.AdjustSeverity(pawn, (HediffDef)this.hediff, 0.5f);
+        }
+
+    }
+}
